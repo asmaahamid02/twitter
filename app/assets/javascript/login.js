0c2647f6165091a06eaa5358777aa5a49674bc2d
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-13 12:13:36 +0300

    declaring consts

---
+++ app/assets/javascript/login.js
@@ -0,0 +1,10 @@
+// DECLARING CONSTANTS
+const signup_btn = document.getElementById('signup-btn');
+const form_body = document.getElementById('form-body');
+const close_form = document.getElementById('close-form');
+const submit_signup =  document.getElementById('submit-signup');
+const form_page_1 = document.getElementById('form-page-1');
+const signin_btn = document.getElementById('signin-btn');
+const signin_form = document.getElementById('signin-form');
+const signin_close_form = document.getElementById('signin-close-form');
+const inner_signup_btn = document.getElementById('inner-signup-btn');
