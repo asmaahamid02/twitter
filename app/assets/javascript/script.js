17493ad1ddac3bd31733e6db2d94e2b1fe498065
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-14 13:55:10 +0300

    implemented buttons functionality

--- app/assets/javascript/script.js
+++ app/assets/javascript/script.js
@@ -1,1 +1,16 @@
 /* Main JS */
+
+let edit_profile_btn = document.getElementById('edit-profile');
+let edit_popup = document.getElementById('edit-popup');
+let close_profile_edit = document.getElementById('close-profile-edit');
+
+
+edit_profile_btn.addEventListener('click', ()=>{
+    edit_popup.style.display = 'block';
+
+});
+
+close_profile_edit.addEventListener('click', ()=>{
+    edit_popup.style.display = 'none';
+    
+});
