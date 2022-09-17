92427234ff689df150a53853d5f61822cafe2257
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-18 01:15:50 +0300

    fix bugs

--- backend/apis/connection.php
+++ backend/apis/connection.php
@@ -2,11 +2,11 @@
 
 $host = "localhost";
 $db_user = "root";
-$db_pass = '';
+$db_pass = '1234';
 $db_name = "twitterdb";
 
 $connection = new mysqli($host, $db_user, $db_pass, $db_name);
 
-if($connection->connect_error){
+if ($connection->connect_error) {
     die('Connection failed: ' + $connection->connect_error);
 }
