419ef97e0672b2f8ebae105fa610813aa1e756c9
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-18 00:10:27 +0300

    change input hidden value from 2 to 0 for privacy option

--- app/assets/javascript/toggle.js
+++ app/assets/javascript/toggle.js
@@ -1,26 +1,21 @@
 // // TOGGLE HOME <-> PROFILE
 
-    const profile_btn = document.getElementById('profile-btn');
-    const main_feed = document.querySelector('.main-feed');
-    const profile_container = document.querySelector('.profile-container');
-    const main_flex = document.querySelector('.main-flex');
-    
-    profile_btn.addEventListener('click', ()=>{
+const profile_btn = document.getElementById('profile-btn')
+const main_feed = document.querySelector('.main-feed')
+const profile_container = document.querySelector('.profile-container')
+const main_flex = document.querySelector('.main-flex')
 
-        main_feed.style.display = 'none';
-        profile_container.style.display = 'flex';
-
-    });
-
-
+profile_btn.addEventListener('click', () => {
+  main_feed.style.display = 'none'
+  profile_container.style.display = 'flex'
+})
 
 // ////
 const showAudienceToggleButton = document.querySelector('#publicity')
 const box = document.querySelector('#publicity-choices-js')
 //toggle the box of publicity choices
 showAudienceToggleButton.addEventListener('click', () => {
-    
-    box.classList.toggle('none');
+  box.classList.toggle('none')
 })
 
 const privacy = document.querySelector('#privacy')
@@ -36,15 +31,17 @@ options.forEach((option) => {
       if (option.id == 'public-choice') {
         private_option.value = 1
         document.querySelector('#private-check').classList.add('none')
-        document.querySelector('#public-check').style.display = 'block';
-        box.classList.toggle('none');
-        showAudienceToggleButton.innerHTML = 'Everyone <i class="material-icons-outlined">expand_more</i>';
+        document.querySelector('#public-check').style.display = 'block'
+        box.classList.toggle('none')
+        showAudienceToggleButton.innerHTML =
+          'Everyone <i class="material-icons-outlined">expand_more</i>'
       } else {
-        private_option.value = 2
+        private_option.value = 0
         document.querySelector('#private-check').classList.remove('none')
-        document.querySelector('#public-check').style.display = 'none';
-        box.classList.toggle('none');
-        showAudienceToggleButton.innerHTML = 'Followers <i class="material-icons-outlined">expand_more</i>'
+        document.querySelector('#public-check').style.display = 'none'
+        box.classList.toggle('none')
+        showAudienceToggleButton.innerHTML =
+          'Followers <i class="material-icons-outlined">expand_more</i>'
       }
     }
   })
