aaceb07704c875a8890f85097b5c5cd161d37449
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-18 12:48:42 +0300

    close #28; create tweet like api and changing include statement in php files

--- backend/apis/connection.php
+++ backend/apis/connection.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $db_user = "root";
-$db_pass = '';
+$db_pass = '1234';
 $db_name = "twitterdb";
 
 $connection = new mysqli($host, $db_user, $db_pass, $db_name);
--- backend/apis/get_user_tweets.php
+++ backend/apis/get_user_tweets.php
@@ -1,5 +1,5 @@
 <?php
-include("connection.php");
+include_once "connection.php";
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
